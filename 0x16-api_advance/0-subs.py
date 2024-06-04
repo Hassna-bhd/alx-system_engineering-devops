5a236812b208081c0189685e922fa5637d50d723
Hassna Bahadou <70654984+Hassna-bhd@users.noreply.github.com>
2024-06-04 21:57:54 +0100

    Update 0-subs.py

--- 0x16-api_advance/0-subs.py
+++ 0x16-api_advance/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-# function that queries the Reddit API and returns the number of subscribers
+""" Exporting csv files"""
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    Get the number of Subscribers
-    """
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-
-    # Request
-    req = requests.get('http://www.reddit.com/r/{}/about.json\
-'.format(subreddit), headers={'User-Agent': '0x16-api_advanced:project: v1.0.0\
-(by /u/kinyarasam)'}).json()
-
-    # Get subs.
-    subs = req.get("data", {}).get("subscribers", 0)
-    return subs
-    
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
+    else:
+        return(0)
